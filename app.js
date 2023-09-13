57d072b468ef5b8d0bf75039114f078f50fdf6ee
dinismario <128180233+dinismario@users.noreply.github.com>
2023-09-13 12:57:18 +0100

    Js

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 
-var nome ="dinis mario";
-var idade =19;
+// var nome ="dinis mario";
+// var idade =19;
 
-// alert(`Ola Mundo , Seja bem vindo ao Portal da Unitel \n Nome: ${nome} \n idade: ${idade}`);
+// // alert(`Ola Mundo , Seja bem vindo ao Portal da Unitel \n Nome: ${nome} \n idade: ${idade}`);
 
-if(idade>=18){
+// if(idade>=18){
 
-    document.write(`A sua idade é ${idade} anos podes votar <br> <br>`);
+//     document.write(`A sua idade é ${idade} anos podes votar <br> <br>`);
 
-}else{
+// }else{
 
-    document.write(`A sua idade é ${idade} anos não podes votar <br><br>`);
+//     document.write(`A sua idade é ${idade} anos não podes votar <br><br>`);
 
-}
+// }
 
-for(i = 1; i <= 12; i++){
+// for(let i = 1; i <= 12; i++){
 
-    const resultado = 7 * 1;
+//     const resultado = 7*i;
 
-    document.write(`7 x ${i} = ${resultado} <br>`); 
-}
+//     document.write(`7 x ${i} = ${resultado}<br>`); 
+// }
 
+
+const botao = document.getElementById('botao');
+
+const resultado= document.getElementById('resultado');
+
+let contador = 0;
+
+botao.addEventListener('click', function(){
+          
+
+});
