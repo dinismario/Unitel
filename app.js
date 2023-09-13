7097c9ac7abe9bfb273a42367d4d2b0caa793843
dinismario <128180233+dinismario@users.noreply.github.com>
2023-09-13 12:27:41 +0100

    codigos

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var nome ="dinis mario";
+var idade =19;
+
+// alert(`Ola Mundo , Seja bem vindo ao Portal da Unitel \n Nome: ${nome} \n idade: ${idade}`);
+
+if(idade>=18){
+
+    document.write(`A sua idade é ${idade} anos podes votar <br> <br>`);
+
+}else{
+
+    document.write(`A sua idade é ${idade} anos não podes votar <br><br>`);
+
+}
+
+for(i = 1; i <= 12; i++){
+
+    const resultado = 7 * 1;
+
+    document.write(`7 x ${i} = ${resultado} <br>`); 
+}
+
